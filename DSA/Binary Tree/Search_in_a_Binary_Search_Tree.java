999496e8d5c1ea95df384e449d969a9d60d36917
Sagar Dutta <duttasagar086@gmail.com>
2023-06-16 20:32:03 +0530

    Binary Search Tree questions

---
+++ DSA/Binary Tree/Search_in_a_Binary_Search_Tree.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Search_in_a_Binary_Search_Tree {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if(root==null)
+            return null;
+        if(root.val==val)
+            return root;
+        else if(root.val>val)
+            return searchBST(root.left, val);
+        else
+            return searchBST(root.right, val);
+    }
+}
