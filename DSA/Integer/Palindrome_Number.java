6a292e00b090c3e357d6ab2b036cb6b5d25950ea
Sagar Dutta <duttasagar086@gmail.com>
2023-06-24 21:03:55 +0530

    Integer Question

---
+++ DSA/Integer/Palindrome_Number.java
@@ -0,0 +1,13 @@
+class Palindrome_Number {
+    public boolean isPalindrome(int x) {
+        if(x<0)
+            return false;
+        int rev = 0;
+        int num = x;
+        while(x!=0){
+            rev = rev*10 + (x%10);
+            x = x/10;
+        }
+        return rev==num; 
+    }
+}
