478a78529ff4f3084dc9b07ae35fc205dab4c621
Sagar Dutta <duttasagar086@gmail.com>
2023-06-29 18:02:51 +0530

    Array Question

---
+++ DSA/Array/Cyclically_rotate_an_array_by_one.java
@@ -0,0 +1,9 @@
+class Cyclically_rotate_an_array_by_one {
+    public void rotate(int arr[], int n)
+    {
+        int temp = arr[n-1];
+        for(int i = n-2; i>=0; i--)
+            arr[i+1] = arr[i];
+        arr[0] = temp;
+    }
+}
