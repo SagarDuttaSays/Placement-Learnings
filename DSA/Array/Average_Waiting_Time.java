3002b0224d926749ea241d4b8ba048c41b759db3
Sagar Dutta <107404861+SagarDuttaSays@users.noreply.github.com>
2023-08-09 23:34:04 +0530

    Update Average_Waiting_Time.java

--- DSA/Array/Average_Waiting_Time.java
+++ DSA/Array/Average_Waiting_Time.java
@@ -1,6 +1,7 @@
 
 class Average_Waiting_Time {
     public double averageWaitingTime(int[][] customers) {
+        
         double currTime = customers[0][0], wt = 0;
         for(int i = 0; i<customers.length; i++){
             if(currTime<customers[i][0])
