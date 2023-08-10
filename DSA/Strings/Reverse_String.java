49abcff63477becf047868bbc6b12d3825920ec8
Sagar Dutta <107404861+SagarDuttaSays@users.noreply.github.com>
2023-08-10 23:26:14 +0530

    Create Reverse_String.java

---
+++ DSA/Strings/Reverse_String.java
@@ -0,0 +1,10 @@
+class Reverse_String {
+      public void reverseString(char[] s) {
+        int i = 0, j = s.length-1;
+        while(i<j){
+            char temp = s[i];
+            s[i++] = s[j];
+            s[j--] = temp;
+        }
+    }
+}
