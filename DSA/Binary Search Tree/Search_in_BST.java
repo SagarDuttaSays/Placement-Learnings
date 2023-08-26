948952aa059cdf97549e911c00df234ce392f7fb
Sagar Dutta <107404861+SagarDuttaSays@users.noreply.github.com>
2023-08-26 23:32:45 +0530

    Create Search_in_BST.java

---
+++ DSA/Binary Search Tree/Search_in_BST.java
@@ -0,0 +1,39 @@
+class Search_in_BST {
+     public static boolean searchInBST(BinaryTreeNode root, int x) {
+        // Write your code here.
+        if(root==null)
+            return false;
+        if(root.data==x)    
+            return true;
+        if(x<root.data)
+           return searchInBST(root.left, x);
+        else if(x>root.data)   
+           return searchInBST(root.right,x);
+        return false;
+    }
+}
+/*********************************************************
+
+ Following is the BinaryTreeNode structure:
+
+ class BinaryTreeNode {
+     int data;
+     BinaryTreeNode left;
+     BinaryTreeNode right;
+     BinaryTreeNode() {
+         this.data = 0;
+         this.left = null;
+         this.right = null;
+     }
+     BinaryTreeNode(int data) {
+         this.data = data;
+         this.left = null;
+         this.right = null;
+     }
+     BinaryTreeNode(int data, BinaryTreeNode left, BinaryTreeNode right) {
+         this.data = data;
+         this.left = left;
+         this.right = right;
+     }
+ };
+ ********************************************************/
