47d682936dfae5e8f6e5b089b1ab196455e216a5
Sagar Dutta <107404861+SagarDuttaSays@users.noreply.github.com>
2023-09-01 23:36:00 +0530

    Update Pair_whose_sum_is_equal_to_given_value.java

--- DSA/Hashing/Pair_whose_sum_is_equal_to_given_value.java
+++ DSA/Hashing/Pair_whose_sum_is_equal_to_given_value.java
@@ -1,6 +1,6 @@
 import java.util.*;
-public class Pair_whose_sum_is_equal_to_given_value{
-    public static boolean pair_whose_sum_is_equal_to_given_value(int[] nums, int sum){
+public class Pair_whose_sum_is_equal_to_given_value {
+    public static boolean pair_whose_sum_is_equal_to_given_value(int[] nums, int sum) {
         Set<Integer> set = new LinkedHashSet<>();
         for(int i = 0; i<nums.length; i++)
             set.add(nums[i]);
