fe91dd061d5cf53c3c58b7d18016c203b13c7724
Sagar Dutta <107404861+SagarDuttaSays@users.noreply.github.com>
2023-09-03 18:18:09 +0530

    Create Array_Subset_of_another_array.java

---
+++ DSA/Array/Array_Subset_of_another_array.java
@@ -0,0 +1,16 @@
+class Array_Subset_of_another_array {
+      public static boolean checkSubset(int[] arr1, int[] arr2, int n, int m) {
+        // Write your code here.
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        int i = 0, j = 0;
+        while(i<n && j<m){
+            if(arr1[i]==arr2[j])
+                j++;
+            i++;
+        }
+        if(j==m)
+            return true;
+        return false;
+    }
+}
