c93853d4e4601b432200d8547ad8762c6060fbb1
Sagar Dutta <107404861+SagarDuttaSays@users.noreply.github.com>
2023-09-07 21:44:28 +0530

    Create Sort_Colors.java

---
+++ DSA/Array/Sort_Colors.java
@@ -0,0 +1,19 @@
+//DUTCH NATIONAL FLAG
+class Sort_Colors {
+      public void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    public void sortColors(int[] nums) {
+        int low = 0, mid = 0, high = nums.length-1;
+        while(mid<=high){
+            if(nums[mid]==0)
+                swap(nums, mid++, low++);
+            else if(nums[mid]==1)
+                mid++;
+            else if(nums[mid]==2)
+                swap(nums, mid, high-- );
+        }
+    }
+}
