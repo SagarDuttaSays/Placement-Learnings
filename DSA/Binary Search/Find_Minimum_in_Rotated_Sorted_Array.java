f5d3f8ad1dea7ba28cb29d73c31a067aaed2a938
Sagar Dutta <107404861+SagarDuttaSays@users.noreply.github.com>
2023-09-29 22:45:58 +0530

    Update Find_Minimum_in_Rotated_Sorted_Array.java

--- DSA/Binary Search/Find_Minimum_in_Rotated_Sorted_Array.java
+++ DSA/Binary Search/Find_Minimum_in_Rotated_Sorted_Array.java
@@ -2,7 +2,7 @@ class Find_Minimum_in_Rotated_Sorted_Array {
     public int findMin(int[] nums) {
         int min = Integer.MAX_VALUE;
         int left = 0, right = nums.length-1;
-        while(left<=right){
+        while(left<=right){ 
             int mid = (left+right)/2;
             if(nums[mid]<min)
                 min=nums[mid];
