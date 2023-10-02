fd8c3517829543791739f7233d9ee41544149f26
Sagar Dutta <107404861+SagarDuttaSays@users.noreply.github.com>
2023-10-02 23:47:59 +0530

    Update Nth higest salary.sql

--- DBMS/SQL/Nth higest salary.sql
+++ DBMS/SQL/Nth higest salary.sql
@@ -1,4 +1,4 @@
-select distinct salary from employee order by salary desc limit 1 offset n-1; 
+select distinct salary from employee order by salary desc limit 1 offset n-1;
 
 //if asked to print null in case there is no nth highest value
 
